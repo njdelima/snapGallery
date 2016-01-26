e7e5290bda46771440a0bb6f60497ff0f3d43583
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-26 17:25:30 -0500

    add options - optional

--- snapGallery.js
+++ snapGallery.js
@@ -4,17 +4,22 @@
 		return console.warn("snapGallery needs jQuery!");
 	}
 	
-	$.fn.snapGallery = function() {
+	$.fn.snapGallery = function(options) {
 		var ul = this;
 
+		var settings = $.extend({
+			margin: "10px",
+			backgroundColor: "blue"
+		}, options);
 
 
 		ul.css("box-sizing", "border-box").children("*").css("box-sizing", "border-box");
 
 		ul.css({
-			'list-style-type': 'none',
-			'padding': '0',
-			'margin': '0'
+			list-style-type: 'none',
+			padding: '0',
+			margin: '0',
+			backgroundColor: settings.backgroundColor
 		});
 
 		return this;
