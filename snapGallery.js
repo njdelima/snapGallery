15b591455741df4613647022bb26936fdfce596d
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-26 19:41:28 -0500

    positioning the imgs absolutely

--- snapGallery.js
+++ snapGallery.js
@@ -9,26 +9,20 @@
 	});
 
 	$.fn.snapGallery = function(options) {
-		var ul = this;
-
-
-		ul.children("li").each(function (index) {
-			console.log( "index = " + index);
-		});
 		
-		var lis = ul.children("li");
-
-
-
 		var settings = $.extend({
 			margin: "10",
 			backgroundColor: "blue"
 		}, options);
 
+		var containerWidth = this.width();
 
-		ul.css("box-sizing", "border-box").children("*").css("box-sizing", "border-box");
+		var topTracker = [0, 0, 0];
 
-		ul.css({
+		this.css("box-sizing", "border-box").children("*").css("box-sizing", "border-box");
+
+		this.css({
+			'position': 'relative',
 			'list-style-type': 'none',
 			'padding': '0',
 			'margin': '0',
@@ -36,25 +30,38 @@
 			'width': '100%'
 		});
 
-		console.log("ul.width = " + ul.width());
-		console.log("settings.margin = " + settings.margin);
-
-		var liWidth = (ul.width() / 3);
-		// - (2 * settings.margin);
-
-		console.log("liWidth = " + liWidth);
-
-		ul.children("li").css({
+		this.children("li").css({
 			'display': 'inline-block',
-			'width': liWidth,
-			'margin-right': '-4px'
-			//'margin-right': (settings.margin - 4) + 'px',
-			//'margin-left': settings.margin + 'px'
+			'width': containerWidth / 3,
+			'margin-right': '-4px',
+			'position': 'absolute'
 		}).children("img").css({
 			'width': '100%',
 			'display': 'block'
 		});
 
+		this.children("li").each(function (index) {
+			console.log( "index = " + index);
+			if (index % 3 === 0) {
+				this.css({
+					'top': topTracker[0],
+					'left': 0
+				});
+				topTracker[0] = topTracker[0] + this.height();
+			} else if (index % 3 === 1) {
+				this.css({
+					'top': topTracker[1],
+					'left': this.width()
+				});
+				topTracker[1] = topTracker[1] + this.height();
+			} else if (index % 3 === 2) {
+				this.css({
+					'top': topTracker[2],
+					'left': this.width() * 2
+				});
+				topTracker[2] = topTracker[2] + this.height();
+			}
+		});
 
 		return this;
 	};
