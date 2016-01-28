213c63defdf0e34ac3de2e98be67b98dcfae60fd
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-28 16:12:40 -0500

    min col number is 1

--- snapGallery.js
+++ snapGallery.js
@@ -38,7 +38,7 @@
 			console.log("currentCols = " + currentCols);
 			var imgWidth = (containerWidth / currentCols) - (settings.margin * ( (currentCols - 1) / currentCols));
 			console.log("imgWidth = " + imgWidth);
-		} while (imgWidth < settings.minWidth);
+		} while ((imgWidth < settings.minWidth) && (currentCols > 1));
 
 		console.log("currentCols = " + currentCols);
 		console.log("imgWidth = " + imgWidth);
